0c416c26d3622fe9e1b81c486f4b0b03d688d817
hugthegrizzly <89808394+hugthegrizzly@users.noreply.github.com>
2026-03-05 23:06:24 +0100

    Move back to Eleventy
    with PagesCMS

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("src/style.css");
+    eleventyConfig.addPassthroughCopy("src/assets");
+
+    return {
+
+        dir: {
+        input: 'src',
+        output: '_public',
+        includes: '_partials',
+        layouts: '_layouts'
+        }
+    };
+
+};
