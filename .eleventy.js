d01a65f75971c2652aaf75232f017595dd9fcf27
hugthegrizzly <89808394+hugthegrizzly@users.noreply.github.com>
2026-03-06 19:56:23 +0100

    Custom auto "recent posts" collection
    11ty config. Also added an output directory sweep rule on every rebuild, to not have obsolete files

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,12 @@ module.exports = function(eleventyConfig) { // Function to export src into publi
 
     eleventyConfig.addFilter("postDate", (dateObj) => { // Add custom 11ty filter to reformat dates (using Luxon)
         return DateTime.fromJSDate(dateObj).toFormat('d LLL yyyy') // Take YAML date and format it
-    })
+    });
+
+    eleventyConfig.addCollection("recentPost", (collectionsApi) =>
+    {
+        return collectionsApi.getFilteredByTag("post").reverse().slice(0,3); // Use 11ty's collections object and its methods to filter by the "post" tag, reverse it's chronological order, and limit (aka slice) the resulting array
+    });
 
     return {
 
@@ -16,7 +21,8 @@ module.exports = function(eleventyConfig) { // Function to export src into publi
             output: '_public', // Public site export
             includes: '_partials', // Partials AKA components/slices/snippets
             layouts: '_layouts', // Layouts AKA templates
-        }
+        },
+        cleanOutputDir: true // 
     };
 
 };
--- src/blog/2026-03-06-spongebub.md
+++
@@ -1,8 +0,0 @@
----
-thumbnail: /assets/blog/first-post/thumbnail-placeholder.png
-date: 2026-03-06
-title: Spongebub
-published: false
-description: Uhhhhhhhh who lives under the sea
----
-Spongebob square pants!
